e684f15dabdd48f14a440e5c20404451e6ef5be9
FC <czacha223@gmail.com>
2019-11-10 21:25:45 +0100

    New project

--- Emptyfiles.py
+++ Emptyfiles.py
@@ -1,6 +1,27 @@
 from tkinter import *
 
+#window setup
 window = Tk()
-windowName = Label(window, text='Emptyfiles generator')
-windowName.pack()
+variable = StringVar(window)
+variable.set('one')
+optionMenu = OptionMenu(window, variable, "one", "two")
+
+ledgerLabel = Label(window, text="Ledger")
+businessDateLabel = Label(window, text="Business Date YYYYMMDD")
+timeStampDate = Label(window, text="TimeStamp Date YYYYMMDD")
+sequenceNumberLabel = Label(window, text='Sequence number')
+
+
+sequenceNumberEntry = Entry(window)
+businessDateLabelEntry = Entry(window)
+timeStampDateEntry = Entry(window)
+
+ledgerLabel.grid(row=0,sticky=E)
+businessDateLabel.grid(row=1, sticky=E)
+timeStampDate.grid(row=2, sticky=E)
+sequenceNumberLabel.grid(row=3, sticky=E)
+optionMenu.grid(row=0, column=1)
+businessDateLabelEntry.grid(row=1, column=1)
+timeStampDateEntry.grid(row=2, column=1)
+sequenceNumberEntry.grid(row=3,column=1)
 window.mainloop()
