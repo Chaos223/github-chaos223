97a2b320946d75136995fbdf582c31442726f4d7
FC <czacha223@gmail.com>
2019-11-11 12:09:12 +0100

    Added button

--- Emptyfiles.py
+++ Emptyfiles.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
-#window setup
+
+def generate():
+    pass
+
+
+# window setup
 window = Tk()
+window.title('Empty files generator')
 variable = StringVar(window)
 variable.set('one')
 optionMenu = OptionMenu(window, variable, "one", "two")
@@ -10,18 +16,19 @@ ledgerLabel = Label(window, text="Ledger")
 businessDateLabel = Label(window, text="Business Date YYYYMMDD")
 timeStampDate = Label(window, text="TimeStamp Date YYYYMMDD")
 sequenceNumberLabel = Label(window, text='Sequence number')
-
+generateButton = Button(window, text='Generate', command=generate)
 
 sequenceNumberEntry = Entry(window)
 businessDateLabelEntry = Entry(window)
 timeStampDateEntry = Entry(window)
 
-ledgerLabel.grid(row=0,sticky=E)
+ledgerLabel.grid(row=0, sticky=E)
 businessDateLabel.grid(row=1, sticky=E)
 timeStampDate.grid(row=2, sticky=E)
 sequenceNumberLabel.grid(row=3, sticky=E)
 optionMenu.grid(row=0, column=1)
 businessDateLabelEntry.grid(row=1, column=1)
 timeStampDateEntry.grid(row=2, column=1)
-sequenceNumberEntry.grid(row=3,column=1)
+sequenceNumberEntry.grid(row=3, column=1)
+generateButton.grid(row=4, columnspan=2)
 window.mainloop()
